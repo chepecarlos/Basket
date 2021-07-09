3e3c02765ffee2e89009c791e2223d90ed784f03
chepecarlos <chepecarlos@alswblog.org>
2021-07-09 16:01:49 -0600

    Mejorando sistema con nuevas fucinoes para trabajando con blender

--- Basket.py
+++ Basket.py
@@ -1,41 +1,35 @@
-#!/usr/bin/python3
+#!/home/chepecarlos/5.Programas/2.Heramientas/3.Basket/venv/bin/python
 # -*- coding: utf-8 -*-
 
-import os
+import logging
 import argparse
+import os
+
+from operaciones.IconoFolder import ActualizarIconoFolder
+from operaciones.OperacionesBlender import CrearProxy
+
+from extra.FuncionesLogging import ConfigurarLogging
+
+logger = logging.getLogger(__name__)
+ConfigurarLogging(logger)
 
 parser = argparse.ArgumentParser(description='Heramienta Asignar iconos a folder')
-parser.add_argument('--icono', '-i', help="Nombre el icono")
+parser.add_argument('--icono', '-i', help="Actualizar Icono a folder gtk")
+parser.add_argument('--blenderproxy', '-bp', help="Creando proxy de Blender", action="store_true")
 
-
-def ActualizarIconoFolder(Icono=".icono.png"):
-    """Asignar un icono a folder recursicamente para gtk."""
-    cantidad = 0
-
-    FolderIgnorar = [".git", "venv", "node_modules", "BL_proxy", "bpsrender"]
-
-    Ruta_Actual = os.getcwd()
-
-    for ruta, directorios, archivos in os.walk(Ruta_Actual, topdown=True):
-
-        for Folder in FolderIgnorar:
-            if Folder in directorios:
-                directorios.remove(Folder)
-
-        for Archivo in archivos:
-            if Archivo == Icono:
-                cantidad += 1
-                print(f"Asignando icono {Icono} a {ruta}")
-                os.system(f"gio set {ruta} metadata::custom-icon {Icono}")
-
-    print(f"Cantidad iconos asignados {cantidad}")
-
+parser.add_argument('--file', '-f', help="Archivo trabajar")
 
 if __name__ == "__main__":
 
     args = parser.parse_args()
-    print("Iniciando programa Basket")
+    logger.info("Iniciando programa Basket")
     if args.icono:
-        ActualizarIconoFolder(args.icono)
+        if args.file:
+            ActualizarIconoFolder(args.file)
+        else:
+            ActualizarIconoFolder()
+    elif args.blenderproxy:
+        print("Empezando a crear proxy")
+        CrearProxy(os.getcwd())
     else:
-        ActualizarIconoFolder()
+        print("Opcion no encontrada, lee documentacion con -h")
