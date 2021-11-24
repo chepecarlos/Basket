ec797eade077fa48f2aa8397ac9156afefa02966
chepecarlos <chepecarlos@alswblog.org>
2021-11-24 17:30:01 -0600

    Agregando grrafica suma

--- src/basket/main.py
+++ src/basket/main.py
@@ -9,34 +9,30 @@ from operaciones.IconoFolder import ActualizarIconoFolder
 from operaciones.Pantillas import CrearFolderVideo, CrearArticulo
 from operaciones.OperacionesBlender import CrearProxy, RenderizarVideo, BorrarTemporalesBender, SuvirVideo
 from operaciones.Video import ConvertirVideo
+from operaciones.graficaSun import graficaSun
 
 
 def main():
     logger = MiLibrerias.ConfigurarLogging(__name__)
 
-    parser = argparse.ArgumentParser(
-        description='Heramientas Automatizacion de ALSW')
+    parser = argparse.ArgumentParser(description="Heramientas Automatizacion de ALSW")
+    parser.add_argument("--icono", "-i", help="Actualizar Icono a folder gtk", action="store_true")
+
+    parser.add_argument("--blender_proxy", "-bp", help="Creando proxy de Blender", action="store_true")
+    parser.add_argument("--blender_renderizar", "-br", help="Renderizar video con Blender", action="store_true")
+    parser.add_argument("--blender_borrar", "-bb", help="Borrar Temporales de Blender", action="store_true")
     parser.add_argument(
-        '--icono', '-i', help="Actualizar Icono a folder gtk", action="store_true")
+        "--blender_completo", "-bc", help="Renderiza video Blender y sube a youtube", action="store_true"
+    )
 
-    parser.add_argument('--blender_proxy', '-bp',
-                        help="Creando proxy de Blender", action="store_true")
-    parser.add_argument('--blender_renderizar', '-br',
-                        help="Renderizar video con Blender", action="store_true")
-    parser.add_argument('--blender_borrar', '-bb',
-                        help="Borrar Temporales de Blender", action="store_true")
-    parser.add_argument('--blender_completo', '-bc',
-                        help="Renderiza video Blender y sube a youtube", action="store_true")
+    parser.add_argument("--video", "-v", help="Convertir video a 60 fps", action="store_true")
+    parser.add_argument("--grafica", "-g", help="Crecar Grafica 7 y 30 Dias")
 
-    parser.add_argument(
-        '--video', '-v', help="Convertir video a 60 fps", action="store_true")
+    parser.add_argument("--proyectovideo", "-p", help="Crear Folder proyecto de Video")
+    parser.add_argument("--proyectoarticulo", "-a", help="Crear articulo base")
 
-    parser.add_argument('--proyectovideo', '-p',
-                        help="Crear Folder proyecto de Video")
-    parser.add_argument('--proyectoarticulo', '-a', help="Crear articulo base")
-    
-    parser.add_argument('--file', '-f', help="Archivo trabajar")
-    parser.add_argument('--depuracion', '-d', help="Activar depuracion", action="store_true")
+    parser.add_argument("--file", "-f", help="Archivo trabajar")
+    parser.add_argument("--depuracion", "-d", help="Activar depuracion", action="store_true")
 
     args = parser.parse_args()
     if args.icono:
@@ -56,8 +52,8 @@ def main():
             RenderizarVideo(args.file)
     elif args.blender_borrar:
         logger.info("Borrar temporales de Blender")
-        BorrarTemporalesBender('BL_proxy')
-        BorrarTemporalesBender('bpsrender')
+        BorrarTemporalesBender("BL_proxy")
+        BorrarTemporalesBender("bpsrender")
     elif args.proyectovideo:
         logger.info(f"Nombre del folder {args.proyectovideo}")
         CrearFolderVideo(args.proyectovideo)
@@ -68,6 +64,8 @@ def main():
     elif args.video:
         if args.file:
             ConvertirVideo(args.file)
+    elif args.grafica:
+        graficaSun(args.grafica)
     else:
         logger.info("Opcion no encontrada, lee documentacion con -h")
         # TODO: Agregar convertir video a 60 full HD
---
+++ src/operaciones/graficaSun.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def graficaSun(Archivo):
+
+    data = pd.read_csv(Archivo)
+    etiquetaFecha = data.columns[0]
+
+    data[etiquetaFecha] = pd.to_datetime(data[etiquetaFecha])
+    data.sort_values(etiquetaFecha, inplace=True)
+
+    etiqueta = data.columns[1]
+    fechas = data[etiquetaFecha]
+    valores = data[etiqueta]
+
+    sum7 = []
+    sum30 = []
+    cantidad = len(valores)
+    for id in range(len(valores)):
+        sum7.append(0)
+        sum30.append(0)
+        for j in range(id - 6, id + 1):
+            if j >= 0 and j < cantidad:
+                sum7[id] += valores[j]
+        for j in range(id - 29, id + 1):
+            if j >= 0 and j < cantidad:
+                sum30[id] += valores[j]
+
+    fig, axs = plt.subplots(3, 1)
+
+    axs[0].plot(fechas, valores, label=etiqueta)
+    axs[0].plot(fechas, sum7, label=f"Suma7 {etiqueta}")
+    axs[0].plot(fechas, sum30, label=f"Suma30 {etiqueta}")
+    axs[0].grid(axis="y", color="gray", linestyle="dashed")
+    axs[0].set_xlabel(etiquetaFecha)
+    axs[0].set_ylabel(etiqueta)
+    axs[0].legend(loc="upper left")  
+    
+
+    axs[1].plot(fechas, valores, label=etiqueta)
+    axs[1].plot(fechas, sum7, label=f"Suma7 {etiqueta}")
+    axs[1].grid(axis="y", color="gray", linestyle="dashed")
+    axs[1].set_xlabel(etiquetaFecha)
+    axs[1].set_ylabel(etiqueta)
+    axs[1].legend(loc="upper left")
+
+    axs[2].plot(fechas, valores, label=etiqueta)
+    axs[2].grid(axis="y", color="gray", linestyle="dashed")
+    axs[2].set_xlabel(etiquetaFecha)
+    axs[2].set_ylabel(etiqueta)
+    axs[2].legend(loc="upper left")
+
+
+    plt.gcf().autofmt_xdate()
+
+    plt.tight_layout()
+    fig.suptitle(f"Grafica suma7 y suma30 de {etiqueta}")
+    plt.show()
+
