b081191a65f0393596e92de1fa3887819eb1ca6c
chepecarlos <chepecarlos@alswblog.org>
2023-12-30 23:03:36 -0600

    reformatiar nombres

--- src/basket/operaciones/IconoFolder.py
+++ src/basket/operaciones/IconoFolder.py
@@ -5,29 +5,33 @@ import basket.miLibrerias as miLibrerias
 logger = miLibrerias.ConfigurarLogging(__name__)
 
 
-def ActualizarIconoFolder(Icono=None, Depuracion=False):
+def actualizarIconoFolder(icono=None, depuracion=False):
     """Asignar un icono a folder recursicamente para gtk."""
-    if Icono is None:
-        Icono = ".icono.png"
-    logger.info(f"Usando el Archivo {Icono}")
+    if icono is None:
+        icono = ".icono.png"
+    logger.info(f"Usando el Archivo {icono}")
     cantidad = 0
 
-    FolderIgnorar = [".git", ".local", "venv", "node_modules", "BL_proxy", "bpsrender"]
+    folderIgnorar = [".git", ".local", "venv", "node_modules", "BL_proxy", "bpsrender"]
 
-    Ruta_Actual = os.getcwd()
+    rutaActual = os.getcwd()
 
-    for ruta, directorios, archivos in os.walk(Ruta_Actual, topdown=True):
+    for ruta, directorios, archivos in os.walk(rutaActual, topdown=True):
 
-        for Folder in FolderIgnorar:
-            if Folder in directorios:
-                directorios.remove(Folder)
+        for folder in folderIgnorar:
+            if folder in directorios:
+                directorios.remove(folder)
 
-        for Archivo in archivos:
-            if Archivo == Icono:
+        for archivo in archivos:
+            if archivo == icono:
                 cantidad += 1
-                Comando = f"gio set {ruta} -t string metadata::custom-icon {Icono}"
-                if Depuracion:
-                    logger.info(f"[{Icono}] {ruta}")
+                Comando = f"gio set {ruta} -t string metadata::custom-icon {icono}"
+                if depuracion:
+                    logger.info(f"[{icono}] {ruta}")
                 os.system(Comando)
 
     logger.info(f"Cantidad iconos asignados {cantidad}")
+
+
+def actualizarIconoProyecto():
+    pass
